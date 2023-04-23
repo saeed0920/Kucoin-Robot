7e04897ae55dde8f3bb34524c762d44aa80d70bf
Saeed0920 <sd.eed1381@gmail.com>
2023-04-24 02:08:24 +0430

    install npm and pakage and finish first page with vuetify and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import progressLine from "./components/progress.vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <v-app>
+    <!-- main -->
+    <v-main style="background-color: #000825">
+      <h1 class="text-white text-6xl font-bold text-center">Orders</h1>
+      <p class="text-white text-center mt-10">
+        this page show you the count of succes and the count of fail.
+      </p>
+      <v-container class="d-flex justify-center align-center text-h5">
+        <!-- template of progress -->
+        <progressLine />
+        <!--  -->
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
